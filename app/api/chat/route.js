7175a61f752f882c8f972cb9108f09194135f8fe
kooza-colline <koozacollinz1@gmail.com>
2024-06-22 10:01:24 +0300

    added the flash api

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(request) {
   try {
     const genAI = new GoogleGenerativeAI(process.env.KOOZA);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro-latest" });
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const { search } = await request.json();
     const result = await model.generateContent(search);
     const responseText = await result.response.text();
